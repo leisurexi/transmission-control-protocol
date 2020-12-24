7f5ab2642c29f83658660e5660a1ea28d83dae4f
leisurexi <leisurexi@gmail.com>
2020-12-24 17:53:13 +0800

    ==> 添加依赖

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # transmission-control-protocol
-使用 UDP 实现类似于 TCP 的可靠传输协议 
+使用 Netty 基于 UDP 协议实现类似于 TCP 的可靠传输协议 
---
+++ src/java/com/leisurexi/tcp/server/TcpServer.java
@@ -0,0 +1,13 @@
+package com.leisurexi.tcp.server;
+
+/**
+ * TCP 服务
+ *
+ * @author: leisurexi
+ * @date: 2020-12-24 16:46
+ */
+public class TcpServer {
+
+
+
+}
